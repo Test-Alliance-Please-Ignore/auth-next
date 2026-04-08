8866df5528d6eee0b7cb841e3b7d7a87b3a80ef8
btnmasher <67168637+btnmasher@users.noreply.github.com>
2026-04-08 16:32:48 -0700

    fix import type (#83)

--- apps/ui/src/client/routes/admin/broadcasts.tsx
+++ apps/ui/src/client/routes/admin/broadcasts.tsx
@@ -1,4 +1,4 @@
-import { ExternalLink, Trash2 } from 'lucide-react'
+import { Ban, ExternalLink, Trash2 } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
