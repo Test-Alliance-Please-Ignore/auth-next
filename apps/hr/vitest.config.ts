b7d6ecc05c54569a68f9ccfd3261329baaf6e51d
btnmasher <67168637+btnmasher@users.noreply.github.com>
2026-05-11 15:48:57 -0700

    fix HR applicaiton search

--- apps/hr/vitest.config.ts
+++ apps/hr/vitest.config.ts
@@ -2,9 +2,10 @@ import { defineWorkersProject } from '@cloudflare/vitest-pool-workers/config'
 
 export default defineWorkersProject({
 	test: {
+		include: ['src/test/integration/**/*.test.ts'],
 		poolOptions: {
 			workers: {
-				wrangler: { configPath: `${__dirname}/wrangler.jsonc` },
+				wrangler: { configPath: `${__dirname}/wrangler.test.jsonc` },
 				miniflare: {
 					bindings: {
 						ENVIRONMENT: 'VITEST',
